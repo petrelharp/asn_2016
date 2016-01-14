5a7a004d1981926fa20776cfc2ff97bba5a01372
peter <petrel.harp@gmail.com>
2016-01-14 13:09:40 -0800

    gave talk

--- habitat-speeds.R
+++ habitat-speeds.R
@@ -7,3 +7,18 @@ speed.results <- lapply( speed.files, function (fn) {
             # should be only one thing
             get(ls(this.env),this.env)
     } )
+
+actual.sigmas <- rbind(
+        "central saline" = c(126.2622,856.5154),
+        "central scrub" = c(130.2535,911.4445),
+        "central wash/wetland" = c(113.5158,768.1860),
+        "central woods/chaparral" = c(123.813,809.005),
+        "western saline" = c(123.0742,866.4747),
+        "western scrub" = c(129.6022,906.3858),
+        "western woods/chaparral" = c(126.0158,876.0518),
+        "empidonax" = c(67.93892,952.16597),
+        "eschscholzia" = c(79.13257,988.03524),
+        "xerospermophilus" = c(81.30487,1005.65509),
+        "yucca" = c(76.86104,992.72200)
+    )
+colnames(actual.sigmas) <- c("seed","pollen")
